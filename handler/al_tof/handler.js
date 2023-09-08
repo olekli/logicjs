0a9b1a2224b7bb04abd4204d65021c271ead39c2
Ole Kliemann <mail@olekliemann.de>
2023-09-08 21:09:10 +0000

    Add counter

--- handler/al_tof/handler.js
+++ handler/al_tof/handler.js
@@ -6,15 +6,54 @@ const { getSession } = require('../../session.js');
 const path = require('path');
 const { make_ok, make_err } = require('okljs');
 
-const options = {
-  length: 3,
-  letters_available: ['p', 'q'],
-  letters_required: ['p', 'q'],
-  operators_available: [ 'equivalent', 'follows', 'or', 'and' ],
-  operators_required: [ 'follows' ],
-  negation_probabilities: {
-    atomic: { single: 0.4, double: 0.2 },
-    complex: { single: 0.2, double: 0.0 },
+const games = {
+  level_1: {
+    num_total_questions: 12,
+    question_options: {
+      length: 2,
+      letters_available: ['p', 'q'],
+      letters_required: ['p' ],
+      operators_available: [ 'equivalent', 'follows', 'or', 'and' ],
+      operators_required: [],
+      negation_probabilities: {
+        atomic: { single: 0.4, double: 0.2 },
+        complex: { single: 0.2, double: 0.0 },
+      }
+    },
+    points_required: 12,
+    time_limit: 0
+  },
+  level_2: {
+    num_total_questions: 12,
+    question_options: {
+      length: 3,
+      letters_available: ['p', 'q'],
+      letters_required: ['p', 'q'],
+      operators_available: [ 'equivalent', 'follows', 'or', 'and' ],
+      operators_required: [ 'follows' ],
+      negation_probabilities: {
+        atomic: { single: 0.4, double: 0.2 },
+        complex: { single: 0.2, double: 0.0 },
+      }
+    },
+    points_required: 12,
+    time_limit: 0
+  },
+  level_3: {
+    num_total_questions: 12,
+    question_options: {
+      length: 3,
+      letters_available: ['p', 'q'],
+      letters_required: ['p', 'q'],
+      operators_available: [ 'equivalent', 'follows', 'or', 'and' ],
+      operators_required: [ 'follows' ],
+      negation_probabilities: {
+        atomic: { single: 0.4, double: 0.2 },
+        complex: { single: 0.2, double: 0.0 },
+      }
+    },
+    points_required: 12,
+    time_limit: 0
   }
 };
 
@@ -25,7 +64,8 @@ const get = (session) => {
         path: 'al_tof/asked',
         data: {
           sentence: session.question.sentence,
-          interpretation: session.question.interpretation
+          interpretation: session.question.interpretation,
+          num_current_question: session.num_current_question
         }
       });
     case 'answered':
@@ -36,7 +76,17 @@ const get = (session) => {
           interpretation: session.question.interpretation,
           answer: boolToString(session.answer),
           result: session.result ? 'richtig' : 'falsch',
-          expected: boolToString(session.question.expected)
+          expected: boolToString(session.question.expected),
+          num_current_question: session.num_current_question
+        }
+      });
+    case 'finished':
+      return make_ok({
+        path: 'al_tof/finished',
+        data: {
+          points_required: session.game.points_required,
+          points_achieved: session.points,
+          result: (session.points >= session.game.points_required)
         }
       });
     default:
@@ -45,8 +95,11 @@ const get = (session) => {
 };
 
 const launch = (session, args) => {
+  session.game = games[args.level];
+  session.question = makeQuestion(session.game.question_options);
+  session.num_current_question = 1;
+  session.points = 0;
   session.state = 'asked';
-  session.question = makeQuestion(options);
   return make_ok();
 };
 
@@ -57,6 +110,9 @@ const answer = (session, args) => {
     session.state = 'answered';
     session.answer = args.answer;
     session.result = (session.question.expected === session.answer);
+    if (session.question.expected === session.answer) {
+      session.points++;
+    }
     return make_ok();
   }
 };
@@ -68,9 +124,15 @@ const next = (session, args) => {
     delete session['question'];
     delete session['answer'];
     delete session['result'];
-    session.state = 'asked';
-    session.question = makeQuestion(options);
-    return make_ok();
+    session.num_current_question++;
+    if (session.num_current_question <= session.game.num_total_questions) {
+      session.state = 'asked';
+      session.question = makeQuestion(session.game.question_options);
+      return make_ok();
+    } else {
+      session.state = 'finished';
+      return make_ok();
+    }
   }
 };
 
