02a1fff75d9af15b0fea044d700e3e4debc2c72d
Ole Kliemann <mail@olekliemann.de>
2023-10-18 18:56:56 +0000

    Cleanup package.json

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 const express = require('express');
-const OpenApiValidator = require('express-openapi-validator');
 const pug = require('pug');
 const path = require('path');
 const { ajv } = require('./validation.js');
